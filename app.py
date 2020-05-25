511c10d454436739efca820b8c13c57b95911d48
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2020-05-25 08:24:36 -0300

    Part two

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from web3 import Web3, HTTPProvider
 
 # truffle development blockchain address
-blockchain_address = 'http://127.0.0.1:9545'
+blockchain_address = 'http://127.0.0.1:8545'
 # Client instance to interact with the blockchain
 web3 = Web3(HTTPProvider(blockchain_address))
 # Set the default account (so we don't need to set the "from" for every transaction call)
@@ -11,7 +11,7 @@ web3.eth.defaultAccount = web3.eth.accounts[0]
 # Path to the compiled contract JSON file
 compiled_contract_path = 'build/contracts/HelloWorld.json'
 # Deployed contract address (see `migrate` command output: `contract address`)
-deployed_contract_address = '0xb7afC8dB8EEf302cd30553B39cEa0599093FDE3C'
+deployed_contract_address = '0x433D7E91B659CB4f1bbBb8dacd2B1cBBa7F82F1A'
 
 with open(compiled_contract_path) as file:
     contract_json = json.load(file)  # load contract info as JSON
@@ -24,3 +24,10 @@ contract = web3.eth.contract(address=deployed_contract_address, abi=contract_abi
 message = contract.functions.sayHello().call()
 
 print(message)
+
+# executes setPayload function
+tx_hash = contract.functions.setPayload('abc').transact()
+# waits for the specified transaction (tx_hash) to be confirmed
+# (included in a mined block)
+tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+print('tx_hash: {}'.format(tx_hash.hex()))
--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,6 +1,12 @@
 pragma solidity >= 0.5.0;
 
 contract HelloWorld {
+    string public payload;
+
+    function setPayload(string memory content) public {
+        payload = content;
+    }
+
     function sayHello() public pure returns (string memory) {
         return 'Hello World!';
     }
